827bc920afae7ddd9c6aec29239f9e69a1a9d78b
Brent Acs <brent.mann@gmail.com>
2021-02-12 19:40:05 -0500

    End of lession 6.56 - added head nav w/ ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  // recipesTab or shoppingListTab
+  selectedTabName: string = 'recipesTab';
+
+  onRecipesClick(tabName: string){
+    console.log('tabName: ' + tabName );
+    this.selectedTabName = 'recipesTab';
+  }
+
+  onShoppingListClick(tabName: string){
+    console.log('tabName: ' + tabName);
+    this.selectedTabName = 'shoppingListTab';
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,36 @@
-import { Component } from "@angular/core";
+import { 
+  Component, 
+  Output, 
+  EventEmitter, 
+  ViewChild, 
+  ElementRef, 
+  AfterViewInit } from "@angular/core";
 
 @Component({
   selector: "app-header",
   templateUrl: "./header.component.html"
 })
 export class HeaderComponent{
-  
+  @Output() recipesClicked = new EventEmitter<string>();
+  @Output() shoppingListClicked = new EventEmitter<string>();
+  @ViewChild('recipesTab', {static: true}) recipesTab: ElementRef;
+  @ViewChild('shoppingListTab', { static: true }) shoppingListTab: ElementRef;
+
+  ngAfterViewInit(): void {
+    this.onRecipesClick();
+  }
+
+  onRecipesClick(){
+    console.log('onRecipesClick');
+    this.recipesClicked.emit('recipesTab');
+    this.recipesTab.nativeElement.style.backgroundColor = 'lightblue';
+    this.shoppingListTab.nativeElement.style.backgroundColor = 'transparent';
+  }
+
+  onShoppingListClick(){
+    console.log('onShoppingListClick');
+    this.shoppingListClicked.emit('shoppingListTab');
+    this.recipesTab.nativeElement.style.backgroundColor = 'transparent';
+    this.shoppingListTab.nativeElement.style.backgroundColor = 'lightblue';
+  }
 }
