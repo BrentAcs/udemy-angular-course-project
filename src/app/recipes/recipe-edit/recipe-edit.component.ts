8af2e039673e97bfea5b2532d39b58e21a763a82
Brent Acs <brent.mann@gmail.com>
2021-02-20 18:07:17 -0500

    EoL 16.230 added ingredient controls to recpie edit

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
 
 import { RecipeService } from '../recipe.service';
@@ -28,26 +28,43 @@ export class RecipeEditComponent implements OnInit {
     });
   }
 
-  onSubmit(){
+  onSubmit() {
     console.log(this.recipeForm.value);
+  }
+
+  get controls() {
+    // a getter!
+    return (<FormArray>this.recipeForm.get("ingredients")).controls;
   }
 
   private initForm() {
     let recipeName = "";
     let recipeImagePath = "";
-    let RecipeDescription = "";
+    let recipeDescription = "";
+    let recipeIngredients = new FormArray([]);
 
     if (this.editMode) {
       const recipe = this.recipeService.getRecipe(this.id);
       recipeName = recipe.name;
       recipeImagePath = recipe.imagePath;
-      RecipeDescription = recipe.description;
+      recipeDescription = recipe.description;
+      if (recipe["ingredients"]) {
+        for (let ingredient of recipe.ingredients) {
+          recipeIngredients.push(
+            new FormGroup({
+              name: new FormControl(ingredient.name),
+              amount: new FormControl(ingredient.amount),
+            })
+          );
+        }
+      }
     }
 
     this.recipeForm = new FormGroup({
       name: new FormControl(recipeName),
       imagePath: new FormControl(recipeImagePath),
-      description: new FormControl(RecipeDescription),
+      description: new FormControl(recipeDescription),
+      ingredients: recipeIngredients,
     });
   }
 }
