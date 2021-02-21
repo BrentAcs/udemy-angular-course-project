8fb66900a325a0712cd9761281b3e705f0063b4a
Brent Acs <brent.mann@gmail.com>
2021-02-21 15:14:33 -0500

    EoL 16.237 moved recipeservice scope to appmodule so we can nav to shopping list and not lose it.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { Recipe } from './recipes/recipe.model';
+
+import { RecipeService } from './recipes/recipe.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
-}
+export class AppComponent {}
